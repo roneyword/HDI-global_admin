cc19b5f39a981f1d8ec92087b1e73f0e1322f70f
roney <roneyword@gmail.com>
2023-07-19 10:00:55 -0300

    ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Menu from "./modules/menu.js";
 import Select from "./modules/select.js";
 import TextArea from "./modules/textArea.js";
+import CopyPaste from "./modules/copypaste.js";
 
 Menu();
 Select();
 TextArea();
+CopyPaste();
