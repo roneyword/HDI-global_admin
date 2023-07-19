0f6101e407d2b351f6412b2176103457f34abd8a
Lucas Costa <lucas.costa5@gmail.com>
2023-07-18 22:04:33 -0300

    input file

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import Menu from "./modules/menu.js";
 import Select from "./modules/select.js";
 import TextArea from "./modules/textArea.js";
+import File from "./modules/file.js";
 
 Menu();
 Select();
 TextArea();
+File();
+
